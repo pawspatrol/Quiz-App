1217dd1b5360c64c220105f6b8f1b7fafc59bd01
pawspatrol <christianosoko@gmail.com>
2026-04-27 17:45:35 +0100

    Delete questions.js

--- questions.js
+++
@@ -1,5 +0,0 @@
-const bibleQuestions = [
-  { question: "Who was the first man?", options: ["Adam", "Eve", "Cain", "Abel"], answer: "Adam" },
-  { question: "Who built the ark?", options: ["Noah", "Moses", "David", "Saul"], answer: "Noah" },
-  { question: "Who is the Son of God?", options: ["Jesus", "Paul", "Peter", "John"], answer: "Jesus" }
-];
